c2eb8a0a5ceaf57084c1e3710a4e36676a2ecae9
mjustinz86 <77868670+mjustinz86@users.noreply.github.com>
2021-02-24 16:05:43 -0600

    Add files via upload

---
+++ Homework_2/Zylab6.22.py
@@ -0,0 +1,14 @@
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+f = int(input())
+
+for x in range(-11,10):
+    for y in range(-11,10):
+        if (a*x+b*y==c) and (d*x+e*y==f):
+            print("\n\nx = ",x," y = ",y)
+            quit()
+print("\nThere is no solution")   
