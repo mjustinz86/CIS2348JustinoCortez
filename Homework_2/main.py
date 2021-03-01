898fedba60f2c61f64fe3b1369ebf2babc4ee83f
mjustinz86 <77868670+mjustinz86@users.noreply.github.com>
2021-03-01 00:58:17 -0600

    Add files via upload
    Part 2

--- Homework_2/main.py
+++ Homework_2/main.py
@@ -1,27 +1,29 @@
 # Justino Cortez ID 1615245
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November',
           'December']
+import_file = open(str(input()), 'r')
+info_file = import_file.readline()
 user_inputs = 0
-while user_inputs != -1:
-    try:
-        user_input = input("Enter the month day, year: ")
-    except IndentationError:
-        continue
-    try:
-        date = user_input.split()
-        amount = len(date)
-        if amount != 3:
-            continue
-        month = date[0]
-        day = date[1]
-        year = date[2]
-        day, comma = day.split(',')
 
-        for x in range(12):
-            if user_input.find(months[x]) >= 0:
-                month = str(x + 1)
-                ans = month + '/' + day + '/' + year
-                print(ans)
-                break
-    except ValueError:
-        continue
+for info_file in import_file:
+    if info_file != -1:
+        try:
+            date = info_file.split()
+            amount = len(date)
+            if amount != 3:
+                continue
+            month = date[0]
+            day = date[1]
+            year = date[2]
+            day, comma = day.split(',')
+
+            for x in range(12):
+                if info_file.find(months[x]) >= 0:
+                    month = str(x + 1)
+                    ans = month + '/' + day + '/' + year
+                    print(ans)
+                    break
+        except ValueError:
+            continue
+    else:
+        break
